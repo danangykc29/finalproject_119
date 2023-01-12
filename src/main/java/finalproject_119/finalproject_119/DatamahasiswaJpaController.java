25108dfc521e9a9843bf1d4facd370f2588e2e3a
Microsoft <Microsoft@DESKTOP-OSQH96F>
2023-01-12 09:31:24 +0700

    memngubah pada entitity manager factory dari null menjadi seperti resistence di meta inf "finalproject_119_finalproject_119_jar_0.0.1-SNAPSHOTPU");

--- src/main/java/finalproject_119/finalproject_119/DatamahasiswaJpaController.java
+++ src/main/java/finalproject_119/finalproject_119/DatamahasiswaJpaController.java
@@ -12,6 +12,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
@@ -25,7 +26,7 @@ public class DatamahasiswaJpaController implements Serializable {
     public DatamahasiswaJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("finalproject_119_finalproject_119_jar_0.0.1-SNAPSHOTPU");
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
