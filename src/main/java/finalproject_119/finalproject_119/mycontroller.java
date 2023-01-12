78ecd623db622811b7537eb13a1ec0bf782f9aa7
Microsoft <Microsoft@DESKTOP-OSQH96F>
2023-01-12 09:32:57 +0700

    membuat my controller

---
+++ src/main/java/finalproject_119/finalproject_119/mycontroller.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package finalproject_119.finalproject_119;
+
+/**
+ *
+ * @author Microsoft
+ */
+public class mycontroller {
+    
+}
