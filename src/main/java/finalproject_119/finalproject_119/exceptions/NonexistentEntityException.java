5cf3d27857bb31d0f85595d516ab56f710c1c023
Microsoft <Microsoft@DESKTOP-OSQH96F>
2023-01-12 09:28:55 +0700

    membuat jpacontroller

---
+++ src/main/java/finalproject_119/finalproject_119/exceptions/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package finalproject_119.finalproject_119.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
