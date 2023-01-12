5f1855298b5a5ca64b4393605ef83b6196591945
Microsoft <Microsoft@DESKTOP-OSQH96F>
2023-01-12 09:27:24 +0700

    membuat entitiy from database dengan nama data mahasiswa

---
+++ src/main/java/finalproject_119/finalproject_119/Datamahasiswa.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package finalproject_119.finalproject_119;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Microsoft
+ */
+//otomatis saat membuat entitiy from database
+@Entity
+@Table(name = "datamahasiswa")
+@NamedQueries({
+    @NamedQuery(name = "Datamahasiswa.findAll", query = "SELECT d FROM Datamahasiswa d"),
+    @NamedQuery(name = "Datamahasiswa.findById", query = "SELECT d FROM Datamahasiswa d WHERE d.id = :id"),
+    @NamedQuery(name = "Datamahasiswa.findByName", query = "SELECT d FROM Datamahasiswa d WHERE d.name = :name"),
+    @NamedQuery(name = "Datamahasiswa.findByNik", query = "SELECT d FROM Datamahasiswa d WHERE d.nik = :nik"),
+    @NamedQuery(name = "Datamahasiswa.findByAddress", query = "SELECT d FROM Datamahasiswa d WHERE d.address = :address")})
+public class Datamahasiswa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "nik")
+    private Integer nik;
+    @Column(name = "address")
+    private String address;
+    @Lob
+    @Column(name = "photo")
+    private byte[] photo;
+
+    public Datamahasiswa() {
+    }
+
+    public Datamahasiswa(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNik() {
+        return nik;
+    }
+
+    public void setNik(Integer nik) {
+        this.nik = nik;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Datamahasiswa)) {
+            return false;
+        }
+        Datamahasiswa other = (Datamahasiswa) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "finalproject_119.finalproject_119.Datamahasiswa[ id=" + id + " ]";
+    }
+    
+}
