7293de2cc7608805c7eb4224aa74530e75289bda
Microsoft <Microsoft@DESKTOP-OSQH96F>
2023-01-12 09:19:41 +0700

    membuat project baru bernama final project serta database baru

---
+++ src/main/java/finalproject_119/finalproject_119/Finalproject119Application.java
@@ -0,0 +1,13 @@
+package finalproject_119.finalproject_119;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finalproject119Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Finalproject119Application.class, args);
+	}
+
+}
---
+++ src/test/java/finalproject_119/finalproject_119/Finalproject119ApplicationTests.java
@@ -0,0 +1,13 @@
+package finalproject_119.finalproject_119;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finalproject119ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
